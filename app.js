ae0a2d3c093da9645435492a4ecd5294517dac7b
Ludovic Saule <ludovic.saule@dci.education>
2023-02-23 09:57:53 +0100

    node project init

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import axios from 'axios';
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+//console.log(process);
+
+app.get('/weather', async (req, res) => {
+	// proxy server
+	// cannot use fetch here - not available inside the node -> axios
+	const response =
+		await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=${process.env.SECRET_KEY}
+    `);
+	// sending response to our client
+	res.send(response.data);
+});
+
+app.listen(PORT, () => {
+	console.log('Server is running on PORT:', PORT);
+});
